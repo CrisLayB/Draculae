21ff845c49ae5d40fb5bcb48b3d2e8e9fe800b4f
Cristian Laynez <54615310+CrisLayB@users.noreply.github.com>
2025-01-25 20:36:25 -0600

    Implementing Spawner allready doned and little changes.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,10 +10,14 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float _z_upper, _z_lower, _x_upper, _x_lower;
 
     // Start is called before the first frame update
-    void Start()
+    public void StartingSpawnCoins()
     {
-
         InvokeRepeating("SpawnNewCoins", 1.0f, 3.0f);
+    }
+
+    public void StopSpawnCoins()
+    {
+        CancelInvoke();
     }
 
     private void SpawnNewCoins() {
@@ -21,11 +25,5 @@ public class Spawner : MonoBehaviour
         float x = UnityEngine.Random.Range(_x_lower, _x_upper);
         float z = UnityEngine.Random.Range(_z_lower, _z_upper);
         Instantiate(_coin_prefab, new Vector3(x, 3.0f, z), Quaternion.identity);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
