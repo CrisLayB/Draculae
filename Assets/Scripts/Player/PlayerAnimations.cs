69221adac708a23b83f4a4b92e4c18ae664edd3c
Cristian Laynez <54615310+CrisLayB@users.noreply.github.com>
2025-01-25 21:58:57 -0600

    Animations for Player.

---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    
+    public void WalkingAnimation(float movement)
+    {
+        animator.SetFloat("movement", movement);
+    }
+
+    public void GetCoin()
+    {
+        animator.SetTrigger("grab");
+    }
+
+    public void ThrowCoin()
+    {
+        animator.SetTrigger("throw");
+    }
+
+    public void DanceChoice()
+    {
+        animator.SetInteger("typeDance", Random.Range(1, 3));
+    }
+}
