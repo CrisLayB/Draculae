1c1a6331d8b555749b2a642cc5615f3dfa344e02
darielVilla01 <dariel.villatoro@outlook.com>
2025-01-25 20:18:18 -0600

    Use random ranges instead of fixed positions

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,29 +7,20 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _coin_prefab;
-    private Vector3[] _coin_positions;
-    private ArrayList _coin_list = new ArrayList();
+    [SerializeField] private float _z_upper, _z_lower, _x_upper, _x_lower;
 
     // Start is called before the first frame update
     void Start()
     {
-        _coin_positions = new Vector3[transform.hierarchyCount];
 
-        for (int i = 0; i < transform.hierarchyCount - 1; i++)
-        {
-            _coin_positions[i] = transform.GetChild(i).position;
-        }
-
-        foreach (Transform child in transform) {
-            GameObject coin = Instantiate(_coin_prefab, child.position, child.rotation);
-        }
-
-        InvokeRepeating("SpawnNewCoins", 3.0f, 3.0f);
+        InvokeRepeating("SpawnNewCoins", 1.0f, 3.0f);
     }
 
     private void SpawnNewCoins() {
-        int random_position = UnityEngine.Random.Range(0, _coin_positions.Length);
-        Instantiate(_coin_prefab, _coin_positions[random_position], Quaternion.identity);
+
+        float x = UnityEngine.Random.Range(_x_lower, _x_upper);
+        float z = UnityEngine.Random.Range(_z_lower, _z_upper);
+        Instantiate(_coin_prefab, new Vector3(x, 3.0f, z), Quaternion.identity);
     }
 
     // Update is called once per frame
